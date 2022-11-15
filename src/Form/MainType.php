d312406bab759924f6a06bc49685fb9f830535b9
Patrick-ENI <formation.eni.psu@gmail.com>
2022-11-15 15:54:11 +0100

    Reste sortie en query

--- src/Form/MainType.php
+++ src/Form/MainType.php
@@ -7,6 +7,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -52,7 +53,14 @@ class MainType extends AbstractType
             ->add('isPassed', CheckboxType::class, [
                 'label'    => 'Sorties passées',
                 'required' => false,
-            ]);
+            ])
+            ->add('search', SubmitType::class, [
+                'label' => 'Rechercher !'
+            ])
+            ->add('create', SubmitType::class, [
+                'label' => 'Créer une sortie !'
+            ])
+        ;
 
     }
 
